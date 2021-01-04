5432e9e2ecf1b97e0a3b185ab518ebda7967b5d6
Hector Diaz <hectordiaz1103@gmail.com>
2021-01-04 18:27:44 -0500

    moved files

--- Schedule.js
+++ Schedule.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
     // GIVEN I am using a daily planner to create a schedule
     // WHEN I open the planner
     // THEN the current day is displayed at the top of the calendar
+    //IF DAY MATCHES TODAYS DATE, DISPLAY CURRENT DAY TIME BLOCKS, AND CLEAR PREVIOUS DAY 
     // WHEN I scroll down
     // THEN I am presented with time blocks for standard business hours
     // WHEN I view the time blocks for that day
     // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+    //WILL NEED 3 IF STATEMENTS. IF THE TIME IS PAST, COLOR, IF THE TIME IS CURRENT COLOR DIFFERENT, IF THE TIME IS FUTURE COLOR DIFFERENT
     // WHEN I click into a time block
     // THEN I can enter an event
     // WHEN I click the save button for that time block
